5c7e834438e13f5b3f0a2afd75e4fd79678910c0
Ujjawal-hub <jainujjawal22@gmail.com>
2025-11-18 19:36:16 +0530

    framework

---
+++ src/main/java/modules/payloadmanager.java
@@ -0,0 +1,114 @@
+package modules;
+
+import POJO.request.resfullbooker.Bookingdates;
+import POJO.request.resfullbooker.Bookingrequest;
+import POJO.response.restfullbooker.Bookingresponse;
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+
+public class payloadmanager {
+
+
+// serelialization
+
+
+  public String sendbookingreques1(){
+
+      Bookingrequest booking = new Bookingrequest();
+
+      booking.setAdditionalneeds("coffe");
+
+      booking.setTotalprice(900l);
+      booking.setDepositpaid(true);
+      booking.setLastname("the poet");
+      booking.setFirstname("dandolien");
+
+
+      Bookingdates bookingdates = new Bookingdates();
+      bookingdates.setCheckin("09-09-1256");
+      bookingdates.setCheckout("11-09-1256");
+
+      booking.setBookingdates(bookingdates);
+
+
+      Gson gson = new Gson();
+
+      String data = gson.toJson(booking);
+      return data;
+  }
+
+
+
+public String sendbookingreques2(){
+
+    Bookingrequest booking = new Bookingrequest();
+
+    booking.setAdditionalneeds("coffe");
+
+    booking.setTotalprice(900l);
+    booking.setDepositpaid(true);
+    booking.setLastname("詩人");
+    booking.setFirstname("タンポポ");
+
+
+    Bookingdates bookingdates = new Bookingdates();
+    bookingdates.setCheckin("09-09-1256");
+    bookingdates.setCheckout("11-09-1256");
+
+    booking.setBookingdates(bookingdates);
+
+
+    Gson gson = new Gson();
+
+    String data = gson.toJson(booking);
+    return data;
+}
+
+    public String sendbookingreques3(){
+
+        Bookingrequest booking = new Bookingrequest();
+        Faker faker = new Faker();
+
+        booking.setAdditionalneeds("coffe");
+
+        booking.setTotalprice(faker.random().nextLong());
+        booking.setDepositpaid(faker.random().nextBoolean());
+        booking.setLastname(faker.name().firstName());
+        booking.setFirstname(faker.name().firstName());
+
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("09-09-1256");
+        bookingdates.setCheckout("11-09-1256");
+
+        booking.setBookingdates(bookingdates);
+
+
+        Gson gson = new Gson();
+
+        String data = gson.toJson(booking);
+        return data;
+    }
+
+
+ // Deserielization
+
+
+ public Bookingresponse recivingbookingresponse(String response){
+
+      Gson gson = new Gson();
+
+     Bookingresponse bookingresponse =  gson.fromJson(response, Bookingresponse.class);
+
+     return bookingresponse;
+
+
+ }
+
+
+
+
+
+}
+
+
--- src/test/java/Assert/Assertactions.java
+++ src/test/java/Assert/Assertactions.java
@@ -1,4 +1,46 @@
 package Assert;
 
+import io.restassured.response.Response;
+import org.testng.Assert;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.testng.Assert.assertEquals;
+
 public class Assertactions {
+
+    public void verifyResponseBody(String expected,String Actual,String description ){
+
+        assertEquals(expected,Actual,description);
+    }
+    public void verifyResponseBody(int expected,int Actual,String description ){
+
+        assertEquals(expected,Actual,description);
+    }
+
+public void verifystatuscode(Response response, Integer Actual){
+
+        assertEquals(response.getStatusCode(),Actual);
+}
+
+public void verifyingKey(String Key,String Actual){
+
+    assertThat(Key).isNotNull().isNotBlank().isEqualTo(Actual);
+
+}
+
+public void verifyingKeynotNull(Integer Key){
+
+        assertThat(Key).isNotNull();
+
+}
+
+public void verifyingKeynotNull(String Key){
+        assertThat(Key).isNotNull();
+
+}
+
+public void verifyTrue(boolean Key){
+
+        Assert.assertTrue(Key);
+}
 }
